77202597d88046a8b15f60122007eeb853f3414a
matrixdecoded <67524452+matrixdecoded@users.noreply.github.com>
2020-07-12 23:13:04 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from nlpwiz.version import Version
 
 
 setuptools.setup(name='nlpwiz',
-                 version=Version('1.1.0').number,
+                 version=Version('1.1.1').number,
                  description='Python Package Boilerplate',
                  long_description=open('README.md').read().strip(),
                  author='Author Name',
@@ -11,6 +11,7 @@ setuptools.setup(name='nlpwiz',
                  url='https://github.com/vymana/nlpwiz',
                  py_modules=['nlpwiz'],
                  install_requires=[],
+                 packages=setuptools.find_packages(),
                  license='MIT License',
                  zip_safe=False,
                  keywords='boilerplate package',
