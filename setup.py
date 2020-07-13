15c4ef287b89e67a218ae7f85f4595d4802987c2
Jay Mishra <jay.kr.mishra@gmail.com>
2020-07-12 21:06:03 -0500

    updating

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from nlpwiz.version import Version
 
 
 setuptools.setup(name='nlpwiz',
-                 version=Version('1.0.0').number,
+                 version=Version('1.1.0').number,
                  description='Python Package Boilerplate',
                  long_description=open('README.md').read().strip(),
                  author='Author Name',
